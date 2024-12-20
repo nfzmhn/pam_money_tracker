feed5c3a0fd8aba7ac0182a4aca3a58b6d1e56b4
nfzmhn <zidninurfauzi2203@gmail.com>
2024-12-20 19:45:38 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Expenses Tracker App
+
+A Daily Expenses Tracker App built using Flutter, Bloc, and Firebase.
+
+## 🚀 Features
+
+- **Beautiful Design**: Crafted with attention to detail using different packages.
+- **State Management**: Utilizes Bloc for efficient and manageable state handling.
+- **Backend Integration**: Powered by Firebase for robust and scalable backend functionality.
+
+## 🛠️ Technology Stack
+
+- **Flutter**: For a seamless cross-platform mobile app experience.
+- **Bloc**: State management to ensure performance and readability.
+- **Firebase**: Backend services for real-time database, authentication, and more.
+
+## 📦 Packages Used
+
+- `fontawesome_flutter`: For beautiful icons.
+- `fl_chart`: For stunning charts and graphs.
+- `intl`: For internationalization and localization.
+- `flutter_colorpicker`: For providing color picker widgets.
+- `firebase_core`: For initializing and configuring Firebase in the app.
+- `bloc`: For implementing the Bloc pattern for state management.
+- `flutter_bloc`: For integrating Bloc with Flutter widgets.
+- `equatable`: For simplifying equality comparisons.
+- `uuid`: For generating unique identifiers.
+- `expense_repository`: Custom package for managing expense data.
+- `redacted`: automatically generate a loading widget from your own widget.
+
+## 📈 Roadmap
+
+1. **Design Completion**: 🎨 Done!
+2. **Backend Integration with firebase**: 🔧 Done!
+3. **Testing and Optimization**: 🧪 Done!
+4. **Deployment**: 🚀 Soon
+
+## 🖼️ Screenshots
+
+![1](https://github.com/azix-khan/Expense-Tracker/assets/138978130/701e2a57-a6af-4a75-889e-5a594e86f357)
+
+Design from Dribble: https://dribbble.com/shots/15560984-Daily-Expense-Tracker/attachments/7344374?mode=media
+
+## 🔍 Getting Started
+
+To get a local copy up and running follow these simple steps:
+
+1. **Clone the repo**:
+   ```sh
+   git clone https://github.com/azix-khan/Expense-Tracker.git
+
+2. **Navigate to project directory**:
+   ```sh
+   cd expense-tracker-app
+
+3. **Install dependencies**:
+   ```sh
+   flutter pub get
+4. **Run the app**:
+   ```sh
+   flutter run
+
+# Contributing 🤝
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+1. Fork the Project
+2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+4. Push to the Branch (git push origin feature/AmazingFeature)
+5. Open a Pull Request
+
+## License
+
+Distributed under the MIT License. See [LICENSE](LICENSE) for more information.
---
+++ lib/screens/home/views/main_screen.dart
@@ -0,0 +1,358 @@
+import 'dart:math';
+
+import 'package:expense_repository/expense_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+
+class MainScreen extends StatelessWidget {
+  final List<Expense> expenses;
+  const MainScreen(this.expenses, {super.key});
+
+  // to show total expenses balance
+  double _calculateTotalExpenses(List<Expense> expenses) {
+    double total = 0;
+    for (var expense in expenses) {
+      total += expense.amount;
+    }
+    return total;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final totalExpenses = _calculateTotalExpenses(expenses);
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+        child: Column(
+          children: [
+            // welcome bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.yellow[700],
+                          ),
+                        ),
+                        const Icon(
+                          CupertinoIcons.person_alt,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).colorScheme.outline,
+                          ),
+                        ),
+                        Text(
+                          "Jonh Doe",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.onSurface,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.settings),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            // card
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 2,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).colorScheme.tertiary,
+                    ],
+                    transform: const GradientRotation(pi / 4),
+                  ),
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4,
+                      color: Colors.grey.shade300,
+                      offset: const Offset(5, 5),
+                    )
+                  ]),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Total Balacnce",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    '\$${totalExpenses.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //income row
+                        Row(
+                          children: [
+                            Container(
+                              width: 25,
+                              height: 25,
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  CupertinoIcons.arrow_up,
+                                  size: 12,
+                                  color: Colors.greenAccent,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Income",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '\$ 2,500.00',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        //expense row
+                        Row(
+                          children: [
+                            Container(
+                              width: 25,
+                              height: 25,
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  CupertinoIcons.arrow_down,
+                                  size: 12,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Expense",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '\$ 8,000.00',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // Transiction row
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Transactions",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "View All",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context).colorScheme.outline,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: expenses.length,
+                itemBuilder: (context, int i) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        color:
+                                            Color(expenses[i].category.color),
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                    Image.asset(
+                                      'assets/${expenses[i].category.icon}.png',
+                                      scale: 2,
+                                      color: Colors.white,
+                                    ),
+                                    // transactionsData[i]['icon'],
+                                    // Icon(
+                                    //   // transactionsData[i]['icon'],
+                                    //   // color: Colors.white,
+                                    // ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 12,
+                                ),
+                                Text(
+                                  expenses[i].category.name,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color:
+                                        Theme.of(context).colorScheme.onSurface,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "\$${expenses[i].amount}.00",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color:
+                                        Theme.of(context).colorScheme.onSurface,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('dd/MM/yyyy')
+                                      .format(expenses[i].date),
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color:
+                                        Theme.of(context).colorScheme.outline,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
